766cf60767041441544a99bcb99bb957a587505f
Gianne <cruzgian008@gmail.com>
2025-09-22 07:29:35 +0800

    enw

---
+++ classes_Wrapper/Main.java
@@ -0,0 +1,10 @@
+package classes_Wrapper;
+
+public class Main {
+    public static void main (String[] args){
+        int n = 1;
+        Integer number = n;
+
+        System.out.println(number);
+    }
+}
