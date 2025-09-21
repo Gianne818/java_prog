439c580d4430ac50386dd46ee97bab4638c66492
Gianne <cruzgian008@gmail.com>
2025-09-22 07:34:56 +0800

    antoher

--- classes_Wrapper/Main.java
+++ classes_Wrapper/Main.java
@@ -1,10 +1,27 @@
 package classes_Wrapper;
 
+//Wrapper classes in Java provide a way to treat primitive data types as objects. 
+//They encapsulate the primitive values and provide useful methods to operate on them.
+//Useful when using collections frameworks like ArrayList<> which accepts only objects
 public class Main {
     public static void main (String[] args){
         int n = 1;
-        Integer number = n;
+        Integer number = n;//autoboxing
 
         System.out.println(number);
+
+        int b = number.intValue();//unboxing
+        System.out.println(b);
+
+        String c = "21";
+        Integer c1 = Integer.parseInt(c);
+        System.out.println(c1);
+
+
+        int d = c1;
+        System.out.println(d);
+
+        String s = Integer.toString(d);
+        System.out.println(s);
     }
 }
