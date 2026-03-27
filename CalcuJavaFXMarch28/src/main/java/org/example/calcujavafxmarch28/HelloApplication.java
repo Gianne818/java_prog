fdd5f22ce453792239318ab21746a8ec0c1a9466
Gianne <cruzgian008@gmail.com>
2026-03-28 04:59:33 +0800

    new

--- CalcuJavaFXMarch28/src/main/java/org/example/calcujavafxmarch28/HelloApplication.java
+++ CalcuJavaFXMarch28/src/main/java/org/example/calcujavafxmarch28/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 350, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
